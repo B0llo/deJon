85152665950368b4f4e8e81074f54f7d31dcc532
Lukas Downes <lukasdownes@gmail.com>
2024-07-24 13:20:33 +0200

    bugFixInEventRendering

--- js/eventRendering.js
+++ js/eventRendering.js
@@ -1,4 +1,4 @@
-;(function () {
+(function () {
   const nextEventContainer = document.querySelector("#next-event-container")
   const pastEventContainer = document.querySelector("#past-event-container")
   const template = document.querySelector("template.event")
@@ -14,68 +14,59 @@
     }
   }
 
+  const eventToHTML = (event) => {
+    const clone = template.content.cloneNode(true)
+    let nameField = clone.querySelector(".event-name")
+    let dateField = clone.querySelector(".event-date")
+    let locationField = clone.querySelector(".event-location")
+    let imageFrame = clone.querySelector("img")
+
+    nameField.innerHTML = event["name"]
+    dateField.innerHTML = new Date(event["date"])
+      .toLocaleString("en-GB", {
+        timeZone: "CET",
+      })
+      .replace(",", " @")
+      .replace(/:(\d+)$/, "")
+    locationField.innerHTML = event["location"]
+    imageFrame.alt = event["name"] + " Poster"
+    imageFrame.src = "../images/" + event["image"]
+    return clone
+  }
   const generateEventCards = async () => {
     try {
       const events = await getEventData()
       // FIGURE OUT HOW TO SPLIT NEXT FROM PAST EVENTS
-      const nextEvents = events.filter(
-        (e) => new Date(e.date).getDate() >= new Date().getDate()
-      )
+      let nextEvents = []
+      let pastEvents = []
+      events.forEach((e) => {
+        let a = new Date(e["date"])
+        let b = new Date() - 7200000
+        if (a < b) {
+          pastEvents.push(e)
+        } else {
+          nextEvents.push(e)
+        }
+      })
       if (nextEvents.length === 0) {
-            const eText = document.createElement("p")
-            eText.innerHTML = "No events found."
-            nextEventContainer.appendChild(eText)
-        }
-      nextEvents.forEach((event) => {
-        const clone = template.content.cloneNode(true)
-        let nameField = clone.querySelector(".event-name")
-        let dateField = clone.querySelector(".event-date")
-        let locationField = clone.querySelector(".event-location")
-        let imageFrame = clone.querySelector("img")
-
-        nameField.innerHTML = event["name"]
-        dateField.innerHTML = new Date(event["date"])
-          .toLocaleString("en-GB", {
-            timeZone: "CET",
-          })
-          .replace(",", " @")
-          .replace(/:(\d+)$/, "")
-        locationField.innerHTML = event["location"]
-        imageFrame.alt = event["name"] + " Poster"
-        imageFrame.src = "../images/" + event["image"]
-
-        nextEventContainer.appendChild(clone)
-      })
+        const eText = document.createElement("p")
+        eText.innerHTML = "No events found."
+        nextEventContainer.appendChild(eText)
+      }
+      nextEvents
+        .sort((a, b) => (a.date < b.date ? -1 : a.date > b.date ? 1 : 0))
+        .map(eventToHTML)
+        .forEach(e => nextEventContainer.appendChild(e))
       if (!isHomePage) {
-        const pastEvents = events.filter(
-          (e) => new Date(e.date).getDate() < new Date().getDate()
-        )
         if (pastEvents.length === 0) {
-            const eText = document.createElement("p")
-            eText.innerHTML = "No events found."
-            pastEventContainer.appendChild(eText)
+          const eText = document.createElement("p")
+          eText.innerHTML = "No events found."
+          pastEventContainer.appendChild(eText)
         }
-       pastEvents.forEach((event) => {
-        const clone = template.content.cloneNode(true)
-        let nameField = clone.querySelector(".event-name")
-        let dateField = clone.querySelector(".event-date")
-        let locationField = clone.querySelector(".event-location")
-        let imageFrame = clone.querySelector("img")
-
-        nameField.innerHTML = event["name"]
-        dateField.innerHTML = new Date(event["date"])
-          .toLocaleString("en-GB", {
-            timeZone: "CET",
-          })
-          .replace(",", " @")
-          .replace(/:(\d+)$/, "")
-        locationField.innerHTML = event["location"]
-        imageFrame.alt = event["name"] + " Poster"
-        imageFrame.src = "../images/" + event["image"]
-
-        nextEventContainer.appendChild(clone)
-      })
- 
+        pastEvents
+          .sort((a, b) => (a.date < b.date ? -1 : a.date > b.date ? 1 : 0))
+          .map(eventToHTML)
+          .forEach(e => pastEventContainer.appendChild(e))
       }
     } catch (e) {
       const eText = document.createElement("p")
