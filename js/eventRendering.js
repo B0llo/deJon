9f37dc62f59f3c84e5a8e96d1c0a60f183437f65
Lukas Downes <lukasdownes@gmail.com>
2024-07-24 12:08:03 +0200

    eventRendering

--- js/eventRendering.js
+++ js/eventRendering.js
@@ -18,8 +18,15 @@
     try {
       const events = await getEventData()
       // FIGURE OUT HOW TO SPLIT NEXT FROM PAST EVENTS
-      const nextEvents = events.filter(e => e)
-      events.forEach((event) => {
+      const nextEvents = events.filter(
+        (e) => new Date(e.date).getDate() >= new Date().getDate()
+      )
+      if (nextEvents.length === 0) {
+            const eText = document.createElement("p")
+            eText.innerHTML = "No events found."
+            nextEventContainer.appendChild(eText)
+        }
+      nextEvents.forEach((event) => {
         const clone = template.content.cloneNode(true)
         let nameField = clone.querySelector(".event-name")
         let dateField = clone.querySelector(".event-date")
@@ -27,13 +34,49 @@
         let imageFrame = clone.querySelector("img")
 
         nameField.innerHTML = event["name"]
-        dateField.innerHTML = event["date"]
-        locationField.innerHTML = event["location"] ?? ""
+        dateField.innerHTML = new Date(event["date"])
+          .toLocaleString("en-GB", {
+            timeZone: "CET",
+          })
+          .replace(",", " @")
+          .replace(/:(\d+)$/, "")
+        locationField.innerHTML = event["location"]
         imageFrame.alt = event["name"] + " Poster"
         imageFrame.src = "../images/" + event["image"]
 
         nextEventContainer.appendChild(clone)
       })
+      if (!isHomePage) {
+        const pastEvents = events.filter(
+          (e) => new Date(e.date).getDate() < new Date().getDate()
+        )
+        if (pastEvents.length === 0) {
+            const eText = document.createElement("p")
+            eText.innerHTML = "No events found."
+            pastEventContainer.appendChild(eText)
+        }
+       pastEvents.forEach((event) => {
+        const clone = template.content.cloneNode(true)
+        let nameField = clone.querySelector(".event-name")
+        let dateField = clone.querySelector(".event-date")
+        let locationField = clone.querySelector(".event-location")
+        let imageFrame = clone.querySelector("img")
+
+        nameField.innerHTML = event["name"]
+        dateField.innerHTML = new Date(event["date"])
+          .toLocaleString("en-GB", {
+            timeZone: "CET",
+          })
+          .replace(",", " @")
+          .replace(/:(\d+)$/, "")
+        locationField.innerHTML = event["location"]
+        imageFrame.alt = event["name"] + " Poster"
+        imageFrame.src = "../images/" + event["image"]
+
+        nextEventContainer.appendChild(clone)
+      })
+ 
+      }
     } catch (e) {
       const eText = document.createElement("p")
       eText.innerHTML = "The events couldn't be loaded."
